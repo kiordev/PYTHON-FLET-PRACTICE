3d962ca1ee1c3b3caf505a407d15ec13d44c6428
kiordev <kiordev@gmail.com>
2023-10-18 15:13:46 +0300

    tech commit

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello world!")
+import flet as ft
+
+def main(page: ft.Page):
+    page.add(
+        ft.Container(
+            width=100,
+            height=100,
+            bgcolor="red"
+        )
+    )
+    
+    
+ft.app(target=main)
